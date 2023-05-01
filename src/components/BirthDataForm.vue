18c19271a251fecdfd0840c93390ef2dc193e402
Marcelle Yanari <yanarimy@gmail.com>
2023-05-01 18:30:15 -0300

    time picker and styles

--- src/components/BirthDataForm.vue
+++ src/components/BirthDataForm.vue
@@ -12,12 +12,15 @@ export default {
       latitude: null,
       longitude: null,
       date: null,
-      hour: null,
+      time: {
+        hours: new Date().getHours(),
+        minutes: new Date().getMinutes(),
+      },
     }
   },
   methods: {
     submit() {
-      // console.log(this.formatDate(this.date));
+      console.log(this.formatDate(this.date));
       console.log(this.latitude, this.longitude);
     },
     getPlace(lat, lng) {
@@ -27,13 +30,18 @@ export default {
     previewFormat(date) {
       const formatted = date.toLocaleDateString(navigator.language)
       return formatted;
-    }
-    // formatDate(date) {
-    //   const dateObject = new Date(date);
-    //   const isoDate = dateObject.toISOString();
-    //   // const formmatedDate = isoDate.replace('.000Z', '.00Z');
-    //   return isoDate.replace('.000Z', '.00Z');
-    // },
+    },
+    formatDate(date) {
+      const fullDateObject = new Date(date);
+      const onlyDate = fullDateObject.toISOString().split('T')[0];
+
+      const { hours, minutes } = this.time;
+
+      const formattedHours = `${hours}`.padStart(2, '0');
+      const formattedMinutes = `${minutes}`.padStart(2, '0');
+      
+      return `${onlyDate}T${formattedHours}:${formattedMinutes}:00.00Z`;
+    },
   },
 }
 </script>
@@ -48,7 +56,7 @@ export default {
         placeholder="Which day?"
       />
       <DatePicker
-        v-model="hour"
+        v-model="time"
         time-picker
         placeholder="What about the time?"
       />
@@ -61,17 +69,17 @@ export default {
     margin-bottom: .5rem;
   }
 
-  /* form > .date-time {
+  form > .date-time {
     display: flex;
     margin-bottom: 1rem;
-  } */
+  }
 
-  /* form > .date-time > :first-child {
-    margin-right: 1rem;
-  } */
+  form > .date-time > :first-child {
+    margin-right: .5rem;
+  }
 
-  /* form .dp__input {
+  form .dp__input {
     padding-top: 7px;
     padding-bottom: 7px;
-  } */
+  }
 </style>
