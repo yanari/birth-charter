e3af88ac9c151911621d366d206e3169d2cc403b
Marcelle Yanari <yanarimy@gmail.com>
2023-05-08 02:18:11 -0300

    .

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -2,44 +2,26 @@
 import Chart from '../components/Chart.vue';
 import { LocalStore } from '../utils/localStorage';
 export default {
-  components: { Chart, Chart },
-  mounted() {
-
-  },
+  components: { Chart },
   data() {
-    return {}
+    return {
+      elements: LocalStore.getData().calculation,
+    }
   },
   methods: {
-    getElementsNamesSet(filter) {
-      const elementsSet = this.elements.filter((item) => item.percentage === filter);
-      return elementsSet.map(item => item.name);
-    },
+
   },
   computed: {
-    elements() {
-      return LocalStore.getData().calculation;
-    },
-    percentages() {
-      return this.elements.map(i => i.percentage);
-    },
-    dominant() {
-      const highest = Math.max(...this.percentages);
-      return this.getElementsNamesSet(highest);
-    },
-    weak() {
-      const lowest = Math.min(...this.percentages);
-      return this.getElementsNamesSet(lowest);
-    },
+
   }
 }
 </script>
 <template>
   <main>
-    <div class="description"></div>
+    <div class="description">
+    </div>
     <div class="chart-area">
       <Chart :elements="elements"/>
-      {{ dominant }}
-      {{  weak }}
     </div>
   </main>
 </template>
